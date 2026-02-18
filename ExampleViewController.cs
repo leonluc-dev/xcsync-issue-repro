ff5a6d8f481e8cce88c3c30497fc126a1d870cc8
leonluc-dev <leonlucardie@gmail.com>
2026-02-18 11:31:59 +0100

    Initial

---
+++ ExampleViewController.cs
@@ -0,0 +1,19 @@
+namespace ReproApp;
+
+public partial class ExampleViewController : UIViewController {
+	public ExampleViewController () : base (nameof (ExampleViewController), null)
+	{
+	}
+
+	public override void ViewDidLoad ()
+	{
+		base.ViewDidLoad ();
+		// Perform any additional setup after loading the view, typically from a nib.
+	}
+
+	public override void DidReceiveMemoryWarning ()
+	{
+		base.DidReceiveMemoryWarning ();
+		// Release any cached data, images, etc that aren't in use.
+	}
+}
